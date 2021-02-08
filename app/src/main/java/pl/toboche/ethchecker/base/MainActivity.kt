f90f1df1ee85a3e5402fa89aa3d3a448c6d2d2ed
toboche <toboche@gmail.com>
2021-02-08 22:21:17 +0100

    Very basic activity+fragment

---
+++ app/src/main/java/pl/toboche/ethchecker/base/MainActivity.kt
@@ -0,0 +1,6 @@
+package pl.toboche.ethchecker.base
+
+import androidx.fragment.app.FragmentActivity
+import pl.toboche.ethchecker.R
+
+class MainActivity : FragmentActivity(R.layout.activity_main)
---
+++ app/src/main/java/pl/toboche/ethchecker/home/ui/HomeFragment.kt
@@ -0,0 +1,6 @@
+package pl.toboche.ethchecker.home.ui
+
+import androidx.fragment.app.Fragment
+import pl.toboche.ethchecker.R
+
+class HomeFragment : Fragment(R.layout.fragment_home)
