93d7edb25376a4483f0789f95b77c066798c541e
toboche <toboche@gmail.com>
2021-02-14 17:12:39 +0100

    Fixing espresso test stability issues, adding injection configuration into UI tests

--- app/src/androidTest/java/pl/toboche/ethchecker/HomeScreenTest.kt
+++ app/src/androidTest/java/pl/toboche/ethchecker/HomeScreenTest.kt
@@ -2,12 +2,17 @@ package pl.toboche.ethchecker
 
 import android.os.StrictMode
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jakewharton.espresso.OkHttp3IdlingResource
 import com.squareup.rx2.idler.Rx2Idler
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import io.reactivex.plugins.RxJavaPlugins
+import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
@@ -16,13 +21,22 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import pl.toboche.ethchecker.base.MainActivity
 import pl.toboche.ethchecker.mockserver.SuccessDispatcher
+import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class HomeScreenTest {
 
     @Rule
     @JvmField
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
 
     val mockWebServer = MockWebServer()
 
@@ -30,6 +44,7 @@ class HomeScreenTest {
     fun setup() {
         //This could be put inside a TestRule as a next step,
         //to be shared with the next testing classes
+        hiltRule.inject()
         mockWebServer.start(8080)
         mockWebServer.dispatcher = SuccessDispatcher()
         StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
@@ -39,6 +54,8 @@ class HomeScreenTest {
         RxJavaPlugins.setInitIoSchedulerHandler(
             Rx2Idler.create("RxJava 2.x IO Scheduler")
         )
+        val okHttp3IdlingResource = OkHttp3IdlingResource.create("OkHttp", okHttpClient)
+        IdlingRegistry.getInstance().register(okHttp3IdlingResource)
     }
 
     @After
